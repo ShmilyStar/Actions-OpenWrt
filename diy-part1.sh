6a5b308e9bd2e6e50f678ca7dd4cd5c22417cd75
ShmilyStar <92926599+ShmilyStar@users.noreply.github.com>
2024-10-28 22:25:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,3 +22,4 @@
 #echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
