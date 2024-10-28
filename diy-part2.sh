b33756b605896bba73af79ae8116e5ae073618e7
ShmilyStar <92926599+ShmilyStar@users.noreply.github.com>
2024-10-28 18:43:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -47,3 +47,12 @@ tar -zxf ./dev.tar.gz
 #curl -sfL -o ./meta.tar.gz https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux-arm64.tar.gz
 #tar -zxf ./meta.tar.gz && mv -f clash clash_meta
 chmod +x ./clash* ; rm -rf ./*.gz
+
+# 取消原主题luci-theme-bootstrap 为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# 修改 argon 为默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 删除原默认主题
+rm -rf package/lean/luci-theme-bootstrap
