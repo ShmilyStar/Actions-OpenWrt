8e923bafb22fbfbd9b22ccf5eee23e56635b228c
ShmilyStar <92926599+ShmilyStar@users.noreply.github.com>
2024-10-28 22:21:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,11 +22,11 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 
 # Add packages
 #git clone -b js https://github.com/gngpp/luci-theme-design package/luci-theme-design
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 #git clone https://github.com/messense/aliyundrive-webdav package/aliyundrive-webdav
 #git clone https://github.com/sirpdboy/netspeedtest package/homebox
-git clone https://github.com/destan19/OpenAppFilter package/openappfilter
-rm -rf feeds/packages/net/open-app-filter
+#git clone https://github.com/destan19/OpenAppFilter package/openappfilter
+#rm -rf feeds/packages/net/open-app-filter
 
 # Modify default WiFi SSID
 #sed -i 's/ImmortalWrt-2.4G/Xiaomi_203/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
@@ -39,16 +39,18 @@ rm -rf feeds/packages/net/open-app-filter
 #sed -i 's/mt7981-fw-20230411/mt7981-fw-20230330/g' package/mtk/drivers/warp/Makefile
 
 # Add OpenClash DEV/TUN core
-cd ./feeds/luci/applications/luci-app-openclash/root/etc/openclash/
-mkdir ./core && cd ./core
-curl -sfL -o ./dev.tar.gz https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux-arm64.tar.gz
-tar -zxf ./dev.tar.gz
+#cd ./feeds/luci/applications/luci-app-openclash/root/etc/openclash/
+#mkdir ./core && cd ./core
+#curl -sfL -o ./dev.tar.gz https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux-arm64.tar.gz
+#tar -zxf ./dev.tar.gz
 #curl -sfL -o ./clash_tun.gz https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux-arm64-2023.08.17-13-gdcc8d87.gz
 #gzip -d clash_tun.gz
 #curl -sfL -o ./meta.tar.gz https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux-arm64.tar.gz
 #tar -zxf ./meta.tar.gz && mv -f clash clash_meta
-chmod +x ./clash* ; rm -rf ./*.gz
+#chmod +x ./clash* ; rm -rf ./*.gz
 
 # Add luci-app-mosdns
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5-lua package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
